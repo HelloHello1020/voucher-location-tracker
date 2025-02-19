18ed22a6a8d27a9e8b31de049ae619035f71084f
HelloHello1020 <hellologisticsthailand@gmail.com>
2025-02-19 22:52:06 +0700

    website v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import { useState } from "react";
 
 function App() {
+  const maesot = ["1111"];
+
+  const mywaddy = [];
+
+  const hpaAn = [];
+
+  const yangon = [];
+
+  const mandalay = [];
+
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleSearch = () => {
+    if (maesot.includes(search)) {
+      setResult(`Your item\n${search}\nis in Maesot`);
+    }
+
+    else if (mywaddy.includes(search)) {
+      setResult(`Your item\n${search}\nis in Myawaddy`);    
+    }
+
+    else if (hpaAn.includes(search)) {
+      setResult(`Your item\n${search}\nis in Hpa-An`);    
+    }
+
+    else if (yangon.includes(search)) {
+      setResult(`Your item\n${search}\nis in Yangon`);    
+    }
+
+    else if (mandalay.includes(search)) {
+      setResult(`Your item\n${search}\nis in Mandalay`);    
+    }
+    
+    else {
+      setResult("Not found in any city");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="overlay"></div>
+
+      <h2>Search for a City</h2>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Enter 4-digit number"
+      />
+      <button onClick={handleSearch}>Search</button>
+      {result && <h1>{result}</h1>}
     </div>
   );
 }
